832bcee074a903909f4aa32e5d0cb48572789d19
Mario Gomez <mgomez15@seattlecentral.edu>
2020-07-30 17:32:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,6 +29,16 @@ function theBeatlesPlay(arrMusicians, arrInstruments){
    
  }
  
+ function iLoveTheBeatles(number) {
+  var arr = [];
+  var i = 0;
+  do {
+    arr.push('I love the Beatles!');
+    i++;
+  } while (i <= number && number < 15);
+  return arr;
+}
+ 
  
  
  
